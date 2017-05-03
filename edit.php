ff76f897652016e9b5c502853b7fa60df24276f4
Doug Byers <doug.r.byers@gmail.com>
2017-05-03 12:54:47 -0400

    Delete Working

--- edit.php
+++ edit.php
@@ -186,22 +186,13 @@ $safeemail = htmlentities($arr[2]);
             <div class='card-action'>
 							<a id = 'like' style='color: #b60046;' >Like</a>
 							<a onclick='func$row[0]()'class = 'right' style='cursor:pointer; color: #b60046;' >$safehashtag </a>
+							
 						</div>
 					</div>
 				</div>
 			</div>
 			";
-								//echo "
-			  						//	<div id='modal1' class='modal'>
-											//	<div class='modal-content'>
-											//		<h4>Are you sure?</h4>
-												//	<p>Are you sure you want to delete this row in the data base, once you do, you cannot return</p>
-												//	</div>
-											//	<div class='modal-footer'>
-											//		<a href=".$_SERVER['PHP_SELF']."?id=".$row[0]." style='background-color: #b60046;' class=' btn modal-action modal-close waves-effect waves-light'>Delete</a>
-											//		<a class = 'btn-flat modal-action modal-close waves-effect waves-green'> Cancel</a>
-											//	</div>
-			  							//</div>";
+								
     		}
 		} else {
     		// print status message
@@ -222,19 +213,8 @@ $safeemail = htmlentities($arr[2]);
 	 		echo "<meta http-equiv='refresh' content='0'>";
 		}
 		// if DELETE pressed, set an id, if id is set then delete it from DB
-		if (isset($_GET['id'])) {
-			// create query to delete record
-			echo $_SERVER['PHP_SELF'];
-    		$query = "DELETE FROM posts WHERE id = ".$_GET['id'];
-			// run the query
-     		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
-			// reset the url to remove id $_GET variable
-			$location = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
-			exit;
-		}
 		// close connection
-		mysqli_close($connection);
+		
 	?>
 
 
@@ -281,40 +261,39 @@ $safeemail = htmlentities($arr[2]);
  		}
  		// see if any rows were returned
  		if (mysqli_num_rows($result) > 0) {
-     		// print them one after another
-     		while($row = mysqli_fetch_row($result)) {
- 					echo"<script>
- 					function func$row[0]() {
- 					document.getElementById('hashform').elements[0].value = '$row[2]';
- 					document.getElementById('hashform').submit();
- 					}
- 					</script>";
-         		echo "<div class='row'>
-         <div class='col s12 m12'>
-           <div class='card darken-3'style='background-color: #7be6b4' >
-             <div class='card-content text'>
-               <span class='card-title' style = 'font-weight: 400; color : #b60046;'>$row[1]</span>
-               <p>$row[3]</p>
-             </div>
-             <div class='card-action'>
- 							<a id = 'like' style='color: #b60046;' >Like</a>
- 							<a onclick='func$row[0]()'class = 'right' style='cursor:pointer; color: #b60046;' >$row[2] </a>
- 						</div>
- 					</div>
- 				</div>
- 			</div>
- 			";
- 								//echo "
- 			  						//	<div id='modal1' class='modal'>
- 											//	<div class='modal-content'>
- 											//		<h4>Are you sure?</h4>
- 												//	<p>Are you sure you want to delete this row in the data base, once you do, you cannot return</p>
- 												//	</div>
- 											//	<div class='modal-footer'>
- 											//		<a href=".$_SERVER['PHP_SELF']."?id=".$row[0]." style='background-color: #b60046;' class=' btn modal-action modal-close waves-effect waves-light'>Delete</a>
- 											//		<a class = 'btn-flat modal-action modal-close waves-effect waves-green'> Cancel</a>
- 											//	</div>
- 			  							//</div>";
+    		// print them one after another
+    		while($row = mysqli_fetch_row($result)) {
+					$safecontent = htmlentities($row[3]);
+					$safehashtag = htmlentities($row[2]);
+					$safeuser = htmlentities($row[1]);
+        		echo "<div class='row'>
+        <div class='col s12 m12'>
+          <div class='card darken-3'style='background-color: #7be6b4' >
+            <div class='card-content text'>
+              <span class='card-title' style = 'font-weight: 400; color : #b60046;'>$safeuser</span>
+              <p>$safecontent</p>
+            </div>
+            <div class='card-action'>
+							<a id = 'like' style='color: #b60046;' >Like</a>
+							<a onclick='func$row[0]()'class = 'right' style='cursor:pointer; color: #b60046;' >$safehashtag </a>
+							<a class='waves-effect waves-light btn red' href='#modal$row[0]'>Delete</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			";
+			echo "
+			  							<div id='modal$row[0]' class='modal'>
+												<div class='modal-content'>
+													<h4>Are you sure?</h4>
+													<p>Are you sure you want to delete this row in the data base, once you do, you cannot return</p>
+													</div>
+												<div class='modal-footer'>
+													<a href=".$_SERVER['PHP_SELF']."?id=".$row[0]." style='background-color: #b60046;' class=' btn modal-action modal-close waves-effect waves-light'>Delete</a>
+													<a class = 'btn-flat modal-action modal-close waves-effect waves-green'> Cancel</a>
+												</div>
+			  							</div>";
+		
      		}
  		} else {
      		// print status message
@@ -326,14 +305,6 @@ $safeemail = htmlentities($arr[2]);
      	$content = $_POST['content'];
 
  		// check to see if user has entered anything
- 		if ($content != "") {
- 	 		// build SQL query
- 			$query = "INSERT INTO posts (user, hashtag, content) VALUES ('$user', '$hashtag', '$content')";
- 			// run the query
-      		$result = mysqli_query($connection,$query) or die ("Error in query: $query. ".mysql_error());
- 			// refresh the page to show new update
- 	 		echo "<meta http-equiv='refresh' content='0'>";
- 		}
  		// if DELETE pressed, set an id, if id is set then delete it from DB
  		if (isset($_GET['id'])) {
  			// create query to delete record
