3a8afa944451dbbaa87c2b2fb67a49874e13b45d
AndyCraig1 <andy.craig@10108.local>
2017-05-03 12:10:31 -0400

    Github

--- edit.php
+++ edit.php
@@ -439,8 +439,11 @@ $safeemail = htmlentities($arr[2]);
 
           </div>
         </div>
+				<p>GitHub is a web-based Git or version control repository and Internet hosting service. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project.[3]
 
+				GitHub offers both plans for private and free repositories on the same account[4] which are commonly used to host open-source software projects.[5] As of April 2017, GitHub reports having almost 20 million users and 57 million repositories,[6] making it the largest host of source code in the world.[7]
 
+			</p>
 
     </div>
     <!-- Table of Contents -->
