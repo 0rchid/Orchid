14d530d23c79b1aefabcc7da81404ea8bb294f1a
AndyCraig1 <andy.craig@10108.local>
2017-05-05 14:16:46 -0400

    Pics

--- edit.php
+++ edit.php
@@ -374,7 +374,7 @@ $(document).ready(function(){
           </div>
           <div class="col s12 m9">
             <h4>Andy Craig</h4>
-            <p>Alan is an Information Systems major with a minor in computer science. He worked at as a Front End Developer at Shift Collaborative this past summer.</p>
+            <p></p>
           </div>
         </div>
 				<div class="row">
@@ -384,9 +384,9 @@ $(document).ready(function(){
 
           </div>
         </div>
-				<p>GitHub is a web-based Git or version control repository and Internet hosting service. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project.[3]
+				<p>GitHub is a web-based Git or version control repository and Internet hosting service. It offers all of the distributed version control and source code management functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project.
 
-				GitHub offers both plans for private and free repositories on the same account[4] which are commonly used to host open-source software projects.[5] As of April 2017, GitHub reports having almost 20 million users and 57 million repositories,[6] making it the largest host of source code in the world.[7]
+				GitHub offers both plans for private and free repositories on the same account which are commonly used to host open-source software projects. As of April 2017, GitHub reports having almost 20 million users and 57 million repositories, making it the largest host of source code in the world.
 
 			</p>
 
